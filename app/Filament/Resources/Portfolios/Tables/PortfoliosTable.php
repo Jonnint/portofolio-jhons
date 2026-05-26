132d6b340c2fcc28f0ad43897a9859b07c0106d4
Jonnint <jonni.nicorna.t@gmail.com>
2026-05-26 12:14:49 +0700

    fix: add imports and change recordActions to actions for Portfolios and Galleries

--- app/Filament/Resources/Portfolios/Tables/PortfoliosTable.php
+++ app/Filament/Resources/Portfolios/Tables/PortfoliosTable.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\Portfolios\Tables;
 
 
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -38,7 +41,7 @@ class PortfoliosTable
             ->filters([
                 //
             ])
-            ->recordActions([
+            ->actions([
                 EditAction::make(),
             ])
             ->toolbarActions([
