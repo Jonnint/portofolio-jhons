e300cb3d9ac9197fbace478c4a3293d78c89bec2
Jonnint <jonni.nicorna.t@gmail.com>
2026-05-26 10:00:35 +0700

    debug: add query-db-rows route

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,14 @@ use App\Http\Middleware\VisitorLogger;
 Route::get('/', [PortfolioController::class, 'index'])->middleware(VisitorLogger::class);
 Route::post('/contact', [PortfolioController::class, 'submitContact'])->name('contact.submit');
 Route::post('/guestbook', [PortfolioController::class, 'submitGuestbook'])->name('guestbook.submit');
+
+Route::get('/query-db-rows', function () {
+    try {
+        return response()->json([
+            'guestbooks' => DB::table('guestbooks')->get(),
+            'messages' => DB::table('messages')->get()
+        ]);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()]);
+    }
+});
